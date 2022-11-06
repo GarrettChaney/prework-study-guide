50f82bd05f81806b47c30b6915eff9fb52c014a5
Garrett Chaney <gbcpursuits@gmail.com>
2022-11-06 21:25:02 +1100

    Forgot to save?

--- assets/script.js
+++ assets/script.js
@@ -21,7 +21,7 @@ function selectTopics() {
     }
 };
 
-
+console.log('Here are the topics we learned through Prework:');
 listTopics()
 console.log('Which topic should we study first?');
 selectTopics()
